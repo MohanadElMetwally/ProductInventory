52ab09cb6b59c594a848dca0f0ca4097bca873de
MohanadElMetwally <mohanadelmetwally@gmail.com>
2025-12-05 13:45:02 +0200

    feat(controllers): Add login controller. Add new endpoints to user and product controllers

--- src/main/java/com/demoweb/ProductInventoryApp/controllers/ProductsController.java
+++ src/main/java/com/demoweb/ProductInventoryApp/controllers/ProductsController.java
@@ -3,6 +3,7 @@ package com.demoweb.ProductInventoryApp.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -13,14 +14,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.demoweb.ProductInventoryApp.DTOs.ApiResponse;
-import com.demoweb.ProductInventoryApp.DTOs.MessageDTO;
-import com.demoweb.ProductInventoryApp.DTOs.ProductDTO;
-import com.demoweb.ProductInventoryApp.DTOs.ProductsDTO;
-import com.demoweb.ProductInventoryApp.Exceptions.ProductNotFoundException;
+import com.demoweb.ProductInventoryApp.annotations.CurrentUser;
+import com.demoweb.ProductInventoryApp.dto.MessageDTO;
+import com.demoweb.ProductInventoryApp.dto.product.ProductCreateDTO;
+import com.demoweb.ProductInventoryApp.dto.product.ProductDTO;
+import com.demoweb.ProductInventoryApp.dto.product.ProductDetailDTO;
+import com.demoweb.ProductInventoryApp.dto.product.ProductUpdateDTO;
+import com.demoweb.ProductInventoryApp.dto.product.ProductsDTO;
+import com.demoweb.ProductInventoryApp.dto.product.ProductsSummaryDTO;
+import com.demoweb.ProductInventoryApp.models.Users;
 import com.demoweb.ProductInventoryApp.services.ProductService;
-
-import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/products")
@@ -30,75 +33,44 @@ public class ProductsController {
     private ProductService productService;
 
     @GetMapping("/")
-    public ResponseEntity<ApiResponse> getProducts() {
-        try {
-            return new ResponseEntity<>(new ProductsDTO(productService.getProducts()),
-                HttpStatus.OK);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new MessageDTO("An error occurred while fetching item: " + e.getMessage()));
-        }
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<ProductsDTO> getProducts() {
+        return ResponseEntity.status(HttpStatus.OK).body(productService.getProducts());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse> getProduct(@PathVariable int id) {
-        try {
-            return new ResponseEntity<>(productService.getProductById(id), HttpStatus.OK);
-        } catch (ProductNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new MessageDTO(e.getMessage()));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new MessageDTO("An error occurred while fetching item: " + e.getMessage()));
-        }
+    public ResponseEntity<ProductDetailDTO> getProduct(@PathVariable int id) {
+        return ResponseEntity.status(HttpStatus.OK).body(productService.getProductById(id));
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<ProductsSummaryDTO> getUserProducts(@PathVariable int id) {
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(productService.getProductsByUserId(id));
     }
 
     @PostMapping("/")
-    public ResponseEntity<ApiResponse> createProduct(@Valid @RequestBody ProductDTO dto) {
-        try {
-            return new ResponseEntity<>(productService.addProduct(dto), HttpStatus.OK);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new MessageDTO("An error occurred while adding item: " + e.getMessage()));
-        }
+    public ResponseEntity<ProductDetailDTO> createProduct(@CurrentUser Users user,
+        @RequestBody ProductCreateDTO dto) {
+        return ResponseEntity.status(HttpStatus.OK).body(productService.addProduct(dto, user));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ApiResponse> updateProducts(@PathVariable int id,
-        @RequestBody ProductDTO prod) {
-        try {
-            return new ResponseEntity<>(productService.updateProduct(id, prod), HttpStatus.OK);
-        } catch (ProductNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new MessageDTO(e.getMessage()));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new MessageDTO("An error occurred while updating item: " + e.getMessage()));
-        }
+    public ResponseEntity<ProductDetailDTO> updateProducts(@PathVariable int id,
+        @RequestBody ProductUpdateDTO prod) {
+        return ResponseEntity.status(HttpStatus.OK).body(productService.updateProduct(id, prod));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<MessageDTO> deleteProducts(@PathVariable int id) {
-        try {
-            productService.deleteProduct(id);
-            return ResponseEntity.status(HttpStatus.OK)
-                .body(new MessageDTO("Item deleted successfully."));
-        } catch (ProductNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new MessageDTO(e.getMessage()));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new MessageDTO("An error occurred while deleting item: " + e.getMessage()));
-        }
+        productService.deleteProduct(id);
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(new MessageDTO("Item deleted successfully."));
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<ApiResponse> patchProducts(@PathVariable int id,
+    public ResponseEntity<ProductDetailDTO> patchProducts(@PathVariable int id,
         @RequestBody ProductDTO prod) {
-        try {
-            return new ResponseEntity<>(productService.patchProduct(id, prod), HttpStatus.OK);
-        } catch (ProductNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new MessageDTO(e.getMessage()));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new MessageDTO("An error occurred while patching item: " + e.getMessage()));
-        }
+        return ResponseEntity.status(HttpStatus.OK).body(productService.patchProduct(id, prod));
     }
 }
